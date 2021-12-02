c79a7977f702d0af839cd4e08b217c238adbc203
Samuel Midgley <midgley.sam@gmail.com>
2021-12-02 18:21:08 +0000

    Updates

--- script.js
+++ script.js
@@ -4,23 +4,33 @@ const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.IcosahedronGeometry(.7, 4);
 
 // Materials
 
 const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+material.color = new THREE.Color(0xf4ab32)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
 scene.add(sphere)
 
+const geometry1 = new THREE.IcosahedronGeometry(.2, 4)
+const material1 = new THREE.MeshPhongMaterial()
+material1.color = new THREE.Color(0xff0000)
+const sphere1 = new THREE.Mesh(geometry1, material1)
+sphere1.position.set(-1,-.3,.4)
+scene.add(sphere1)
+
+
+
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff, 1)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
 scene.add(pointLight)
 
 /**
@@ -50,7 +60,7 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 2
@@ -81,7 +91,7 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    sphere1.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
